3ff1b5b5ca5c49ac5650ece3f557b315300e4033
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-07-02 13:19:41 -0300

    FEAT: Criação da model Disciplina. #11

--- backend/gestao_escolar/models.py
+++ backend/gestao_escolar/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Disciplina (models.Model):
+    nome = models.CharField(max_length=45)
+    descricao = models.CharField(max_length=45)
+    carga_horaria = models.IntegerField()
+    curso = models.ForeignKey(Curso, on_delete=models.PROTECT)
+    professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
+    
+    def _str_(self):
+         return f'{self.nome} - {self.carga_horaria}'
