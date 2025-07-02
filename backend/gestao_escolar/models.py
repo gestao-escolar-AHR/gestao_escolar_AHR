e16f0f9bbd04fb102eb2358fa1aa3b20760ae040
HeloLonghi <heloisa.longhi.ifc@gmail.com>
2025-07-02 13:09:35 -0300

    FEAT: Model Aluno criada #8

--- backend/gestao_escolar/models.py
+++ backend/gestao_escolar/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=45)
+    CPF = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=45)
+    pai = models.CharField(max_length=45)
+    mae = models.CharField(max_length=45)
+    data_nasc = models.DateField
+
+    def __str__(self):
+        return f'{self.nome} ({self.email})'
