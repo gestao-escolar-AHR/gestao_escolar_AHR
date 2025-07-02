e87b6cf2466c1b7f65fb90bbd15217a2d9c9fa5f
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-07-02 13:28:41 -0300

    FEAT: Criação da model Professor. #13

--- backend/gestao_escolar/models.py
+++ backend/gestao_escolar/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Professor (models.Model): 
+    nome = models.CharField(max_length=45)
+    CPF = models.IntegerField(default=0)
+    telefone = models.IntegerField(default=0)
+    email = models.EmailField (max_length=45)
+    data_nasc = models.DateField(null=True)
+    formacao = models.CharField(max_length=45)
+    
+    def _str_(self):
+        return f'{self.nome} - {self.email}'
