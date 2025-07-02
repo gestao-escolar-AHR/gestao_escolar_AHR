2717638dcd169a8a840dedd6b927ae77b560f2d9
AmandaRibeiroLuz <amanda.ribeiro.da.luz.201@gmail.com>
2025-07-02 13:13:49 -0300

    FEAT: Model Nota criada. #12.

--- backend/gestao_escolar/models.py
+++ backend/gestao_escolar/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+    
+class Nota (models.Model):
+    nota = models.DecimalField((""), max_digits=4, decimal_places=2)
+    aluno = models.ForeignKey(Aluno, on_delete=models.PROTECT)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.PROTECT)
+ 
+    def __str__(self):
+         return f'{self.disciplina} - {self.nota}'
